1929ba71e9541b93359ca3135ee59d852d75a3ce
enihsgnir <enihsgnir@gmail.com>
2023-08-14 11:35:45 +0900

    use `cloud_firestore_odm`

--- lib/domains/user/user.dart
+++ lib/domains/user/user.dart
@@ -1,12 +1,18 @@
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user.freezed.dart';
 part 'user.g.dart';
 
+@Collection<User>("users")
+final userRef = UserCollectionReference();
+
+@JsonSerializable(converters: firestoreJsonConverters)
 @freezed
 class User with _$User {
   const factory User({
-    required String id,
+    @Id() required String id,
     required String uid,
   }) = _User;
 
