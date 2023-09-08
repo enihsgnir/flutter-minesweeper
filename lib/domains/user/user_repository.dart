a7c95bd36a0138e607bce0aa5edbc87122fe8f96
enihsgnir <enihsgnir@gmail.com>
2023-09-08 15:37:22 +0900

    pad user nickname with a zero character

--- lib/domains/user/user_repository.dart
+++ lib/domains/user/user_repository.dart
@@ -17,9 +17,12 @@ class UserRepository {
   }
 
   Future<String> _generateNickname() async {
-    const max = 10000;
-    final number = Random().nextInt(max);
+    const width = 4;
+    final max = pow(10, 4).toInt();
+
+    final number = Random().nextInt(max).toString().padLeft(width, "0");
     final nickname = "anonymous$number";
+
     final snapshot = await userRef.whereNickname(isEqualTo: nickname).get();
     if (snapshot.docs.isEmpty) {
       return nickname;
