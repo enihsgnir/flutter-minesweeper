8c89016ac8847de7326e33f405debaf31b1568a9
changhoji <onkkno1@gmail.com>
2023-10-04 11:34:00 +0900

    add ranking section

--- lib/views/leaderboard/models/enums.dart
+++ lib/views/leaderboard/models/enums.dart
@@ -1,5 +1,29 @@
+import 'package:flutter_minesweeper/views/play/play_view.dart';
+
 enum Difficulty {
   easy,
   medium,
-  hard,
+  hard;
+
+  int get row {
+    switch (this) {
+      case Difficulty.easy:
+        return BoardConfig.easy().rowCount;
+      case Difficulty.medium:
+        return BoardConfig.medium().rowCount;
+      case Difficulty.hard:
+        return BoardConfig.hard().rowCount;
+    }
+  }
+
+  int get col {
+    switch (this) {
+      case Difficulty.easy:
+        return BoardConfig.easy().colCount;
+      case Difficulty.medium:
+        return BoardConfig.medium().colCount;
+      case Difficulty.hard:
+        return BoardConfig.hard().colCount;
+    }
+  }
 }
---
+++ lib/views/leaderboard/widgets/score_records.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_minesweeper/domains/game/game.dart';
+import 'package:flutter_minesweeper/views/leaderboard/leaderboard_view.dart';
+import 'package:flutter_minesweeper/views/play/play_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ScoreRecords extends ConsumerWidget {
+  const ScoreRecords({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final difficulty = ref.watch(difficultyNotifierProvider);
+
+    return FirestoreBuilder<GameQuerySnapshot>(
+      ref: gameRef
+          .whereRow(isEqualTo: difficulty.row)
+          .whereCol(isEqualTo: difficulty.col)
+          .limit(10),
+      builder: (context, snapshot, child) {
+        if (snapshot.hasError) return const Text("error");
+        if (!snapshot.hasData) return const Text("Loading...");
+
+        final docs = snapshot.data!.docs;
+        return Column(
+          children: [
+            // at most 10 records
+            ...docs.map(
+              (game) => RecordItem(
+                game: game.data,
+                index: docs.indexOf(game),
+              ),
+            ),
+            // rest are null rows
+            ...List.generate(
+              10 - docs.length,
+              (index) => docs.length + index,
+            ).map((index) => RecordItem(game: null, index: index)),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class RecordItem extends StatelessWidget {
+  final Game? game;
+  final int index;
+
+  const RecordItem({
+    super.key,
+    required this.game,
+    required this.index,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: index.isEven ? closedPrimaryColor : closedSecondaryColor,
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: game != null
+            ? [
+                Text(game!.userId),
+                Text(game!.playTime.toString()),
+                Text(game!.createdAt.toString()),
+              ]
+            : [],
+      ),
+    );
+  }
+}
